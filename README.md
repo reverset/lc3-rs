68e14350cafcae646521a77b94cfb24d6145cee8
reverset <53663385+reverset@users.noreply.github.com>
2026-02-19 00:07:02 -0800

    printing using trap 0x21 and halting (0x25)

---
+++ README.md
@@ -0,0 +1,7 @@
+# LC-3-_rs_
+
+## References
+- https://en.wikipedia.org/wiki/Little_Computer_3
+- https://www.cs.utexas.edu/~fussell/courses/cs310h/lectures/Lecture_10-310h.pdf
+- https://acg.cis.upenn.edu/milom/cse240-Fall05/handouts/Ch09-a.pdf
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Display, Formatter};
+use std::io::{Read, Write};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Register {
@@ -12,15 +13,15 @@ pub enum Register {
     R7,
 }
 
-impl Into<u8> for Register {
-    fn into(self) -> u8 {
-        self as u8
+impl From<Register> for u8 {
+    fn from(val: Register) -> u8 {
+        val as u8
     }
 }
 
-impl Into<usize> for Register {
-    fn into(self) -> usize {
-        self as usize
+impl From<Register> for usize {
+    fn from(val: Register) -> usize {
+        val as usize
     }
 }
 
@@ -31,7 +32,6 @@ pub struct Instruction(pub i16);
 // A lot of repeated code here, cleanup one day
 // DR and SR are always 3 bits.
 impl Instruction {
-
     // all inputs should use at most 3 bits.
     pub fn add(dr: impl Into<u8>, sr1: impl Into<u8>, sr2: impl Into<u8>) -> Self {
         let dr = dr.into();
@@ -52,11 +52,11 @@ impl Instruction {
     }
 
     // imm must only use up to 5 bits
-    pub fn add_imm(dr: impl Into<u8>, sr1: impl Into<u8>, imm: u8) -> Self {
+    pub fn add_imm(dr: impl Into<u8>, sr1: impl Into<u8>, imm: i8) -> Self {
         let dr = dr.into();
         let sr1 = sr1.into();
 
-        assert!(dr < 8 && sr1 < 8 && imm < 32);
+        assert!(dr < 8 && sr1 < 8 && (-8..=7).contains(&imm));
         // 15-12  11-9  8-6     4-0
         // 0001    DR   SR1  1  Imm5
 
@@ -67,7 +67,7 @@ impl Instruction {
 
         instr |= 1 << 5;
 
-        instr |= imm as i16;
+        instr |= Self::convert_immediate_byte_to_nybble(imm) as i16;
 
         Instruction(instr)
     }
@@ -98,11 +98,11 @@ impl Instruction {
     // imm must only use up to 5 bits
     // 15-12  11-9  8-6     4-0
     // 0101    DR   SR1  1  Imm5
-    pub fn and_imm(dr: impl Into<u8>, sr1: impl Into<u8>, imm: u8) -> Self {
+    pub fn and_imm(dr: impl Into<u8>, sr1: impl Into<u8>, imm: i8) -> Self {
         let dr = dr.into();
         let sr1 = sr1.into();
 
-        assert!(dr < 8 && sr1 < 8 && imm < 32);
+        assert!(dr < 8 && sr1 < 8 && (-8..=7).contains(&imm));
 
         let mut instr: i16 = 0b0101 << 12;
 
@@ -111,7 +111,7 @@ impl Instruction {
 
         instr |= 1 << 5;
 
-        instr |= imm as i16;
+        instr |= Self::convert_immediate_byte_to_nybble(imm) as i16;
 
         Instruction(instr)
     }
@@ -153,8 +153,58 @@ impl Instruction {
     // ST 	0011 	SR 	 PCoffset9
     // STI 	1011 	SR 	 PCoffset9
     // STR 	0111 	SR 	 BaseR 	offset6
+
     // TRAP	1111 	0000 trapvect8
+    pub fn trap(vector: u8) -> Self {
+        let mut instr: i16 = 0b1111 << 12;
+
+        instr |= vector as i16;
+
+        Instruction(instr)
+    }
+
+    pub fn get_trap_vector(&self) -> Option<u8> {
+        if ((self.0 as u16) >> 8) == 0b11110000 {
+            Some((self.0 & 0b11111111) as u8)
+        } else {
+            None
+        }
+    }
+
+    // source for the following trap vectors: https://acg.cis.upenn.edu/milom/cse240-Fall05/handouts/Ch09-a.pdf
+    pub fn trap_get_c() -> Self {
+        Self::trap(0x20)
+    }
+
+    pub fn trap_out() -> Self {
+        Self::trap(0x21)
+    }
+
+    pub fn trap_puts() -> Self {
+        Self::trap(0x22)
+    }
+
+    pub fn trap_in() -> Self {
+        Self::trap(0x23)
+    }
+
+    pub fn trap_halt() -> Self {
+        // yes 0x24 is skipped
+        Self::trap(0x25)
+    }
+
     // reserved 1101
+
+    // a nybble is a 4-bit number.
+    fn convert_immediate_byte_to_nybble(imm: i8) -> u8 {
+        let mut value = (imm as u8) & 0b00000111; // mask first 3 bits
+
+        if imm < 0 {
+            value |= 0b00001000; // set 4th bit to 1
+        }
+
+        value
+    }
 
     fn get_dr_sr1_sr2(&self) -> (u8, u8, u8) {
         // & to mask out the rest of the bits
@@ -175,12 +225,8 @@ impl Instruction {
     }
 
     fn get_dr_sr(&self) -> (u8, u8) {
-        (
-            ((self.0 >> 9) & 0b111) as u8,
-            ((self.0 >> 6) & 0b111) as u8,
-        )
+        (((self.0 >> 9) & 0b111) as u8, ((self.0 >> 6) & 0b111) as u8)
     }
-
 
     fn check_bit_5(&self) -> bool {
         ((self.0 >> 5) & 0b1) != 0
@@ -237,24 +283,33 @@ impl Registers {
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum ConditionCode { // hmm i thought there was a carry flag
+enum ConditionCode {
+    // hmm i thought there was a carry flag
     Negative,
     Zero,
     Positive,
 }
 
-pub struct Machine {
+pub struct Machine<'a> {
     registers: Registers,
     memory: Vec<i16>,
     ip: u16, // LC-3 is word addressable.
     condition_code: ConditionCode,
+    halted: bool,
+
+    stdin: Box<dyn Read + 'a>,
+    stdout: Box<dyn Write + 'a>,
 }
 
 // Not sure if the condition code should start as the Zero flag.
 // according to https://www.cs.utexas.edu/~fussell/courses/cs310h/lectures/Lecture_10-310h.pdf it states
 // that exactly one condition code is set at all times. I suppose Zero is a sensible default.
-impl Machine {
-    pub fn new(instructions: &[Instruction]) -> Self {
+impl<'a> Machine<'a> {
+    pub fn new_std(instructions: &[Instruction]) -> Self {
+        Self::new(std::io::stdin(), std::io::stdout(), instructions)
+    }
+
+    pub fn new(read: impl Read + 'a, write: impl Write + 'a, instructions: &[Instruction]) -> Self {
         let mut memory = Vec::from_iter((0..0x3000).map(|_| 0)); // instructions start at 0x3000.
         for inst in instructions {
             memory.push(inst.0);
@@ -265,6 +320,15 @@ impl Machine {
             memory,
             ip: 0x3000,
             condition_code: ConditionCode::Zero,
+            halted: false,
+            stdin: Box::new(read),
+            stdout: Box::new(write),
+        }
+    }
+
+    pub fn run_until_halt(&mut self) {
+        while !self.halted {
+            self.step();
         }
     }
 
@@ -288,6 +352,10 @@ impl Machine {
         // ...
         else if instr.is_not() {
             self.handle_not(instr);
+        }
+        // ...
+        else if let Some(vec) = instr.get_trap_vector() {
+            self.handle_trap(vec);
         }
     }
 
@@ -329,7 +397,6 @@ impl Machine {
 
             self.set_condition_code_based_on(dr.into());
         }
-
     }
 
     fn handle_not(&mut self, instr: Instruction) {
@@ -340,30 +407,58 @@ impl Machine {
         self.set_condition_code_based_on(dr.into());
     }
 
+    fn handle_trap(&mut self, vec: u8) {
+        match vec {
+            0x20 => todo!(),
+            0x21 => {
+                let r0 = self.registers.get(Register::R0);
+                self.stdout
+                    .write_all(&[r0 as u8])
+                    .expect("Failed to write to stdout");
+            }
+            0x22 => todo!(),
+            0x23 => todo!(),
+            0x25 => {
+                self.halted = true;
+            }
+            _ => todo!(),
+        }
+    }
+
     fn set_condition_code_based_on(&mut self, reg: Register) {
         self.condition_code = match self.registers.get(reg) {
             0 => ConditionCode::Zero,
-            0.. => ConditionCode::Positive,
+            1.. => ConditionCode::Positive,
             ..0 => ConditionCode::Negative,
         }
     }
 }
 
 fn main() {
-    let mut machine = Machine::new(&[
-        Instruction::add_imm(Register::R0, Register::R1, 5), // r0 = 5
-        Instruction::add_imm(Register::R1, Register::R0, 5), // r1 = 10
+    let mut machine = Machine::new_std(&[
+        // largest immediate we can do is 7
+        // yes this can be condensed
+        Instruction::add_imm(Register::R0, Register::R1, 7), // r0 = 7
+        Instruction::add_imm(Register::R1, Register::R1, 7), // r1 = 7
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (14)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (21)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (28)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (35)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (42)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (49)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (56)
+        Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (63)
+        Instruction::add_imm(Register::R0, Register::R0, 2), // r0 = r0 + 2 (65, 'A' in ASCII)
+        Instruction::trap_out(),                             // print r0
+        Instruction::trap_halt(),
     ]);
-
-    machine.step();
-    machine.step();
-
-    println!("{:?}", machine.registers);
+    machine.run_until_halt();
+    println!();
 }
-
 
 #[cfg(test)]
 mod tests {
+    use std::io::BufWriter;
     use super::*;
 
     #[test]
@@ -399,7 +494,7 @@ mod tests {
 
     #[test]
     fn add_add() {
-        let mut machine = Machine::new(&[
+        let mut machine = Machine::new_std(&[
             Instruction::add_imm(Register::R0, Register::R1, 5), // r0 = 5
             Instruction::add_imm(Register::R1, Register::R0, 5), // r1 = 10
         ]);
@@ -413,7 +508,7 @@ mod tests {
 
     #[test]
     fn add_add_and() {
-        let mut machine = Machine::new(&[
+        let mut machine = Machine::new_std(&[
             Instruction::add_imm(Register::R0, Register::R1, 5), // r0 = 5
             Instruction::add_imm(Register::R1, Register::R1, 5), // r1 = 5
             Instruction::and(Register::R2, Register::R0, Register::R1), // r2 = 5 (r0 & r1)
@@ -430,9 +525,9 @@ mod tests {
 
     #[test]
     fn add_not() {
-        let mut machine = Machine::new(&[
+        let mut machine = Machine::new_std(&[
             Instruction::add_imm(Register::R0, Register::R1, 5), // r0 = 5
-            Instruction::not(Register::R1, Register::R0), // r2 = 1111111111111010 = -6 (!r0)
+            Instruction::not(Register::R1, Register::R0),        // r2 = 1111111111111010 = -6 (!r0)
         ]);
 
         machine.step();
@@ -441,5 +536,35 @@ mod tests {
         assert_eq!(machine.registers.get(Register::R0), 5);
         assert_eq!(machine.registers.get(Register::R1), -6);
     }
-}
 
+    #[test]
+    fn print_a() {
+        let mut output = BufWriter::new(Vec::new());
+
+        let mut machine = Machine::new(std::io::stdin(), &mut output, &[
+            // largest immediate we can do is 7
+            // yes this can be condensed
+            Instruction::add_imm(Register::R0, Register::R1, 7), // r0 = 7
+            Instruction::add_imm(Register::R1, Register::R1, 7), // r1 = 7
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (14)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (21)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (28)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (35)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (42)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (49)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (56)
+            Instruction::add(Register::R0, Register::R0, Register::R1), // r0 = r0 + r1 (63)
+            Instruction::add_imm(Register::R0, Register::R0, 2), // r0 = r0 + 2 (65, 'A' in ASCII)
+            Instruction::trap_out(),                             // print r0
+            Instruction::trap_halt(),
+        ]);
+
+        machine.run_until_halt();
+        drop(machine);
+
+        let buf = output.into_inner().unwrap();
+        let output = String::from_utf8(buf).unwrap();
+
+        assert_eq!(output, "A");
+    }
+}
